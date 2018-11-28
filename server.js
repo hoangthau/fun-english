79c96069f3aa9e5f7b8e7466291aae0a52ecc0d6
Thau Nguyen <hoangthau627@gmail.com>
2018-11-28 14:59:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-ddd
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+//get our api
+const api = require('./routes/api');
+
+//get express app
+const app = express();
+
+//connect to db
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://usertpd:usertpd@ds237735.mlab.com:37735/tpd');
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set our api routes
+app.use('/', api);
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3200';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
